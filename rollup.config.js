55489ed7a3d51bb3fea8ec1fe0a0b9421959a044
Sai Nath <saipkmm100@gmail.com>
2025-08-22 00:30:00 +0530

    Fix build system and generate package distribution files
    - Install missing dependencies: @rollup/plugin-terser, tslib
    - Update rollup config to use correct terser import
    - Successfully build package with all output formats
    - Generate TypeScript declarations and source maps
    
    Build outputs:
    - dist/index.js (CommonJS)
    - dist/index.esm.js (ES Module)
    - dist/index.umd.js (UMD)
    - TypeScript declarations (.d.ts files)

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
-import { terser } from 'rollup-plugin-terser'
+import terser from '@rollup/plugin-terser'
 import dts from 'rollup-plugin-dts'
 
 const isProduction = process.env.NODE_ENV === 'production'
